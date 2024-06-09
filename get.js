eefc39c70af346f65f52ae58085a8cc3608c6c06
wartakita <161806668+wartakita@users.noreply.github.com>
2024-06-09 18:01:33 +0700

    Update get.js

--- get.js
+++ get.js
@@ -1,15 +1,20 @@
 // Function to get parameter value from URL
-    function getParameterByName(name, url) {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
-    }
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
-    // Function to encode string to base64
-    function base64Encode(str) {
-        return btoa(str);
-    }
+// Function to encode string to base64
+function base64Encode(str) {
+    return btoa(str);
+}
+
+// Here we pass our streaming source
+const src = {
+    hls: base64Encode(getParameterByName('hls') || '')
+};
